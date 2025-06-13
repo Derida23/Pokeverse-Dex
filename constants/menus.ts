07f9904f24052b9fa4380564ccebdf61bef53276
Derida23 <arianderida@gmail.com>
2025-06-13 16:00:31 +0700

    feat(contants): create: menus variable for sidebar

---
+++ constants/menus.ts
@@ -0,0 +1,19 @@
+export const menus = [
+  {
+    title: 'Main Menu',
+    menu: [
+      { name: 'Pokemons', icon: 'i-lucide-atom', slug: 'pokemons' },
+      { name: 'Compares', icon: 'i-lucide-git-compare-arrows', slug: 'compares' },
+      { name: 'Statistics', icon: 'i-lucide-chart-pie', slug: 'statistics' },
+      { name: 'Bookmarks', icon: 'i-lucide-bookmark-plus', slug: 'bookmarks' }
+    ]
+  },
+  {
+    title: 'Pokemon Data',
+    menu: [
+      { name: 'Evolutions', icon: 'i-lucide-infinity', slug: 'evolutions' },
+      { name: 'Types', icon: 'i-lucide-toy-brick', slug: 'types' },
+      { name: 'Egg Groups', icon: 'i-lucide-paw-print', slug: 'egg-groups' }
+    ]
+  }
+];
