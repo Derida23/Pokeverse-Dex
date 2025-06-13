6e8eddefdc9670970276f383a503bcea275d5219
Derida23 <arianderida@gmail.com>
2025-06-13 16:04:55 +0700

    feat(components): create: Navbar and Sidebar components with responsive design

---
+++ components/Layout/Navbar.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+  const isDarkMode = ref(false);
+  const isMenuOpen = ref(false);
+</script>
+
+<template>
+  <header class="header">
+    <div class="header-wrapper">
+      <div class="header-title">
+        <p class="text-4xl">
+          🤾🏼‍♂️
+        </p>
+        <div class="italic">
+          <p class="header-description">Pokémon</p>
+          <p class="header-description__sub">Verse</p>
+        </div>
+      </div>
+      <div class="flex items-center gap-x-4">
+        <UToggle
+          v-model="isDarkMode"
+          on-icon="i-lucide-moon-star"
+          off-icon="i-lucide-sun"
+          size="lg"
+          color="amber"
+        />
+        <UIcon
+          name="i-lucide-sliders-horizontal"
+          class="header-navigation" 
+          @click="isMenuOpen = !isMenuOpen" 
+        />
+      </div>
+    </div>
+  </header>
+
+  <USlideover v-model="isMenuOpen" :ui="{ width: 'w-screen max-w-64' }">
+    <div class="px-6 pt-24">
+      <NavigationMenu v-model="isMenuOpen" />
+    </div>
+  </USlideover>
+
+  <NavigationMenuMobile v-model="isMenuOpen" />
+</template>
+
+<style scoped lang="postcss">
+.header {
+  @apply sticky top-0 z-[60];
+  @apply bg-white;
+  @apply border border-b-[1.5px] border-b-slate-200;
+
+  &-wrapper {
+    @apply mx-auto px-5 md:px-10 xl:px-20 h-16 max-w-7xl;
+    @apply flex items-center justify-between;
+  }
+
+  &-title {
+    @apply flex items-center gap-x-0.5;
+  }
+
+  &-description {
+    @apply font-normal text-lg pl-0.5;
+
+    &__sub{
+      @apply font-extrabold text-xl !mt-[-8px];
+    }
+  }
+
+  &-navigation {
+    @apply cursor-pointer text-lg text-zinc-800;
+    @apply hidden md:block lg:hidden;
+  }
+}
+</style>
---
+++ components/Layout/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="sidebar">
+    <div class="flex">
+      <nav class="navbar">
+       <NavigationMenu />
+      </nav>
+      <main class="content">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.sidebar {
+  @apply mx-auto px-10 lg:px-20 max-w-7xl;
+}
+.navbar {
+  @apply fixed bottom-12 top-24 lg:pt-2 z-50;
+  @apply w-64;
+  @apply overflow-y-auto;
+  @apply transition-[right];
+  @apply md:top-16 md:z-40;
+  @apply lg:left-[max(0px,calc(50%-37rem))] lg:w-[14.75rem];
+  @apply lg:py-9 lg:pl-8 lg:pr-5 lg:shadow-none -right-64;
+  @apply border-r-[1.5px] border-r-zinc-200;
+}
+
+.content {
+  @apply mx-auto max-w-[56rem] px-3.5 pb-20 pt-8 
+    md:px-6 md:pt-9 
+    lg:mr-[max(0px,calc(50%-37rem))] lg:w-[calc(100%-10rem)] lg:px-8 lg:py-10
+}
+</style>
