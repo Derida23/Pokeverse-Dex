8181ccd747235d3b7535b2f0ad5aaf7312e3c4f1
Derida23 <arianderida@gmail.com>
2025-06-17 11:25:38 +0700

    feat(pages): update: homepage layout with pokémon not found message and interactive button

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Home Page</h1>
+  <div class="homepage">
+    <p class="text-sorry">SORRY</p>
+    <div class="text-wrapper">
+      <h1 class="font-bold text-slate-800 text-[14rem]">4</h1>
+      <img src="/assets/images/pokeball.webp" class="w-48" >
+      <h1 class="font-bold text-slate-800 text-[14rem]">4</h1>
+    </div>
+    <p class="text-not-found">Pokémon not found</p>
+    <p class="text-explore">Let’s explore further and catch one!</p>
+    <div class="flex items-center justify-center mt-8">
+      <UButton 
+        color="white" 
+        variant="solid" 
+        size="xl" 
+        class="rounded-full"
+        @click="$router.push('/pokemons')"
+      >
+        Throw Poké Ball 
+      </UButton>
+
+    </div>
+  </div>
 </template>
+
+<style scoped lang="postcss">
+.homepage {
+  @apply m-auto max-w-96 mt-10;
+}
+
+.text {
+  &-sorry {
+    @apply text-center text-red-600 text-4xl font-bold tracking-[1rem];
+  }
+
+  &-wrapper {
+    @apply flex items-center justify-center -mt-[2rem];
+  }
+
+  &-not-found {
+    @apply font-semibold tracking-widest text-slate-800 -mt-[1rem] text-center text-xl;
+  }
+
+  &-explore {
+    @apply text-center text-slate-500 font-mono text-xs mt-2;
+  }
+}
+
+</style>
