482a490f200c1be971dbd21909fe39ef9007d3be
Derida23 <arianderida@gmail.com>
2025-06-17 11:25:50 +0700

    feat(app): create: site title configuration and dynamic title template

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+const siteTitle = config.public.siteTitle
+
+useHead({
+  titleTemplate: titleChunk =>
+    titleChunk ? `${titleChunk} - ${siteTitle}` : siteTitle,
+})
+</script>
+
 <template>
    <NuxtLayout>
     <NuxtPage />
