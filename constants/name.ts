21dd02e494b4e230e251f816b3c5081c3c6ca87c
Derida23 <arianderida@gmail.com>
2025-06-19 10:13:02 +0700

    feat(names): create: add japanesePokemonNames constant for Pokémon names in Japanese

---
+++ constants/name.ts
@@ -0,0 +1,21 @@
+export const japanesePokemonNames = [
+  'ピカチュウ',   // Pikachu
+  'フシギダネ',   // Bulbasaur
+  'ヒトカゲ',     // Charmander
+  'ゼニガメ',     // Squirtle
+  'イーブイ',     // Eevee
+  'カビゴン',     // Snorlax
+  'ゲンガー',     // Gengar
+  'リザードン',   // Charizard
+  'ルカリオ',     // Lucario
+  'ニャース',     // Meowth  
+  'アチャモ',     // Torchic  
+  'チコリータ',   // Chikorita  
+  'ミジュマル',   // Oshawott  
+  'エモンガ',     // Emolga  
+  'ラルトス',     // Ralts  
+  'ツタージャ',   // Snivy  
+  'ヤヤコマ',     // Fletchling  
+  'ヒノアラシ',   // Cyndaquil  
+  'コリンク'      // Shinx
+]
