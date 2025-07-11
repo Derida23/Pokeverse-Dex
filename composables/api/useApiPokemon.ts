d0d09611016f6f2d520f9bc23e3d3d053160201a
Derida23 <arianderida@gmail.com>
2025-07-11 15:05:11 +0700

    feat(api): update: unused japanese names logic and simplify getAll function

--- composables/api/useApiPokemon.ts
+++ composables/api/useApiPokemon.ts
@@ -1,64 +1,19 @@
-import { japanesePokemonNames } from "~/constants/names"
+import type { FilterKey, FilterValue } from "@/types/components/Pokemons"
+import type { ApiResponse } from "@/types/responses/ApiResponse"
 import type { Pokemons } from "@/types/responses/Pokemons"
-import type { FilterKey, FilterValue } from "~/types/components/Pokemons"
 
 export function useApiPokemon(){
-  const nameMap = useState('japaneseNames', () => new Map<number, string>())
-
-  function getStableRandomName(id: number): string {
-    if (!nameMap.value.has(id)) {
-      const index = Math.floor(Math.random() * japanesePokemonNames.length)
-      nameMap.value.set(id, japanesePokemonNames[index])
-    }
-
-    return nameMap.value.get(id)!
-  }
-
-
-  const getPokemons = async (offset=0, limit=21, filter: Record<FilterKey, FilterValue>) => {
+  const getPokemons = async (offset=0, limit=21, filter: Record<FilterKey, FilterValue>)
+  : Promise<ApiResponse<Pokemons[]>> => {
     return await $fetch(`/api/pokemons?offset=${offset}&limit=${limit}`, 
       { method: 'GET', query: filter }
     )
   }
 
   const getAll = async (offset=0, limit=21, filter: Record<FilterKey, FilterValue>) => {
-
-    const apiUrl  = 'https://pokeapi.co/api/v2'
-    const { data } = await getPokemons(offset, limit, filter)
-
-    const pokemon = await Promise.all(
-      data.map(name => $fetch(`${apiUrl}/pokemon/${name.name}`))
-    ) as Pokemons[]
-
-    for (const item of pokemon) {
-      const formPokemon = data.find(p => p.id === item.id)
-      item['form'] = formPokemon?.form || []
-      item['title'] = formPokemon?.title || []
-
-      delete item.abilities
-      delete item.base_experience
-      delete item.cries
-      delete item.forms
-      delete item.game_indices
-      delete item.height
-      delete item.held_items
-      delete item.is_default
-      delete item.location_area_encounters
-      delete item.moves
-      delete item.order
-      delete item.past_abilities
-      delete item.past_types
-      delete item.species
-      delete item.stats
-      delete item.weight
-      delete item.sprites
-
-      item.japanese_name = getStableRandomName(item.id)
-    }
-
-    return pokemon
+    const { data, pagination } = await getPokemons(offset, limit, filter)
+    return { data, pagination }
   }
-  
 
   return {
     getAll,
