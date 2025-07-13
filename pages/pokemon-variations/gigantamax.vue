394b7390a6935e24615d6e67ea5ea948910fd8f8
Derida23 <arianderida@gmail.com>
2025-07-13 13:15:45 +0700

    feat(pages): add: gigantamax pages pokemon

--- pages/pokemon-variations/gigantamax.vue
+++ pages/pokemon-variations/gigantamax.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+
+const { getGigantamax } = useApiVariation()
+const { data: pokemons } = await getGigantamax()
+
+</script>
 <template>
-  <h1>Gigantamax</h1>
+  <div>
+    <h1 class="mb-6 text-2xl font-bold">Pokémon Gigantamax</h1>
+    <div class="pokemon">
+      <Card :pokemons="pokemons" :variants="'gmax'" />
+    </div>
+  </div>
 </template>
+
+<style scoped lang="postcss">
+.pokemon {
+  @apply grid w-full grid-cols-3 gap-2;
+}
+</style>
