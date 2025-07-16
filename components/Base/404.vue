2fd11fa1ab71fa15a2c137a12080b014a586e928
Derida23 <arianderida@gmail.com>
2025-07-16 10:18:00 +0700

    feat(components): add: 404 based pages to reuse component

---
+++ components/Base/404.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="homepage">
+    <div class="text-wrapper">
+      <h1 class="font-bold text-slate-800 text-[10rem] md:text-[14rem]">4</h1>
+      <img src="/assets/images/pokeball.webp" class="w-40 md:w-48" >
+      <h1 class="font-bold text-slate-800 text-[10rem] md:text-[14rem]">4</h1>
+    </div>
+    <p class="text-not-found">Pokémon not found</p>
+    <p class="text-explore">Let’s explore further and catch one!</p>
+    <div class="flex items-center justify-center mt-8">
+      <UButton 
+        color="white" 
+        variant="solid" 
+        size="xl" 
+        class="rounded-full"
+        @click="$router.push('/pokemons')"
+      >
+        Reset Filter
+      </UButton>
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.homepage {
+  @apply m-auto max-w-96 mt-10;
+}
+
+.text {
+  &-sorry {
+    @apply text-center text-red-600 text-4xl font-bold tracking-[1rem];
+  }
+
+  &-wrapper {
+    @apply flex items-center justify-center md:-mt-[2rem];
+  }
+
+  &-not-found {
+    @apply font-semibold tracking-widest text-slate-800 md:-mt-[1rem] text-center text-xl;
+  }
+
+  &-explore {
+    @apply text-center text-slate-500 font-mono text-xs mt-2;
+  }
+}
+
+</style>
