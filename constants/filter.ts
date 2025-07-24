38f55b939988696e3502c4b6de087699f860590e
Derida23 <arianderida@gmail.com>
2025-07-24 14:37:28 +0700

    feat(types): update: filter pokemon types and filter column offense and defense pokemon

--- constants/filter.ts
+++ constants/filter.ts
@@ -143,3 +143,122 @@ export const forms =
   { label: 'Mega Form', value: 'is_mega', id: 4 },
   { label: 'GMax Form', value: 'is_gmax', id: 5 },
 ]
+
+
+export const columnsDefense = [
+  { key: 'immune', label: '0x' },
+  { key: 'very_resist', label: '0.25x' },
+  { key: 'resist', label: '0.5x' },
+  { key: 'normal', label: '1x' },
+  { key: 'weak', label: '2x' },
+  { key: 'very_weak', label: '4x' }
+];
+
+export const columnsOffense = [
+  { key: 'weak', label: '2x' },
+  { key: 'normal', label: '1x' },
+  { key: 'resist', label: '0.5x' },
+  { key: 'immune', label: '0x' },
+];
+
+
+export const typesPokemon = [
+  [ {
+      label: "None",
+      name: "none",
+      id: 1
+    }
+  ],
+  [
+    {
+      label: "Normal",
+      name: "normal",
+      id: 1
+    },
+    {
+      label: "Fighting",
+      name: "fighting",
+      id: 2
+    },
+    {
+      label: "Flying",
+      name: "flying",
+      id: 3
+    },
+    {
+      label: "Poison",
+      name: "poison",
+      id: 4
+    },
+    {
+      label: "Ground",
+      name: "ground",
+      id: 5
+    },
+    {
+      label: "Rock",
+      name: "rock",
+      id: 6
+    },
+    {
+      label: "Bug",
+      name: "bug",
+      id: 7
+    },
+    {
+      label: "Ghost",
+      name: "ghost",
+      id: 8
+    },
+    {
+      label: "Steel",
+      name: "steel",
+      id: 9
+    },
+    {
+      label: "Fire",
+      name: "fire",
+      id: 10
+    },
+    {
+      label: "Water",
+      name: "water",
+      id: 11
+    },
+    {
+      label: "Grass",
+      name: "grass",
+      id: 12
+    },
+    {
+      label: "Electric",
+      name: "electric",
+      id: 13
+    },
+    {
+      label: "Psychic",
+      name: "psychic",
+      id: 14
+    },
+    {
+      label: "Ice",
+      name: "ice",
+      id: 15
+    },
+    {
+      label: "Dragon",
+      name: "dragon",
+      id: 16
+    },
+    {
+      label: "Dark",
+      name: "dark",
+      id: 17
+    },
+    {
+      label: "Fairy",
+      name: "fairy",
+      id: 18
+    },
+  ]
+]
--- constants/types.ts
+++ constants/types.ts
@@ -1,4 +1,3 @@
-
 export const types = [
   {
     "name": "normal",
@@ -72,4 +71,115 @@ export const types = [
     "name": "fairy",
     "id": 18
   },
+]
+
+export const pokemon_types = [
+  {
+    "id": 1,
+    "name": "normal",
+    "total_pokemon": 158,
+    "single_pokemon": 138
+  },
+  {
+    "id": 2,
+    "name": "fighting",
+    "total_pokemon": 100,
+    "single_pokemon": 57
+  },
+  {
+    "id": 3,
+    "name": "flying",
+    "total_pokemon": 149,
+    "single_pokemon": 13
+  },
+  {
+    "id": 4,
+    "name": "poison",
+    "total_pokemon": 102,
+    "single_pokemon": 51
+  },
+  {
+    "id": 5,
+    "name": "ground",
+    "total_pokemon": 93,
+    "single_pokemon": 48
+  },
+  {
+    "id": 6,
+    "name": "rock",
+    "total_pokemon": 102,
+    "single_pokemon": 81
+  },
+  {
+    "id": 7,
+    "name": "bug",
+    "total_pokemon": 104,
+    "single_pokemon": 93
+  },
+  {
+    "id": 8,
+    "name": "ghost",
+    "total_pokemon": 92,
+    "single_pokemon": 51
+  },
+  {
+    "id": 9,
+    "name": "steel",
+    "total_pokemon": 91,
+    "single_pokemon": 49
+  },
+  {
+    "id": 10,
+    "name": "fire",
+    "total_pokemon": 103,
+    "single_pokemon": 80
+  },
+  {
+    "id": 11,
+    "name": "water",
+    "total_pokemon": 186,
+    "single_pokemon": 157
+  },
+  {
+    "id": 12,
+    "name": "grass",
+    "total_pokemon": 152,
+    "single_pokemon": 118
+  },
+  {
+    "id": 13,
+    "name": "electric",
+    "total_pokemon": 110,
+    "single_pokemon": 96
+  },
+  {
+    "id": 14,
+    "name": "psychic",
+    "total_pokemon": 136,
+    "single_pokemon": 83
+  },
+  {
+    "id": 15,
+    "name": "ice",
+    "total_pokemon": 66,
+    "single_pokemon": 43
+  },
+  {
+    "id": 16,
+    "name": "dragon",
+    "total_pokemon": 107,
+    "single_pokemon": 52
+  },
+  {
+    "id": 17,
+    "name": "dark",
+    "total_pokemon": 94,
+    "single_pokemon": 59
+  },
+  {
+    "id": 18,
+    "name": "fairy",
+    "total_pokemon": 83,
+    "single_pokemon": 33
+  }
 ]
