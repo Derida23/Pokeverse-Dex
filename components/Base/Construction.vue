d1d391d85a94179bc101cb4979a38b7535b705c0
Derida23 <arianderida@gmail.com>
2025-07-25 06:41:14 +0700

    feat(components): add: under construction page component

---
+++ components/Base/Construction.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-wrap items-center justify-center gap-4 mt-10">
+    <img src="/assets/images/pokemon-under-construction.png" class="w-48">
+    <div class="text-stone-800">
+      <h1 class="text-2xl">We’re in the Tall Grass...</h1>
+      <p class="text-lg">Check Back Soon</p>
+    </div>
+  </div>
+</template>
