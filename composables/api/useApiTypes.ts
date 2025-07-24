00599d451d5352e9633bbb4f57586e55f8174df8
Derida23 <arianderida@gmail.com>
2025-07-24 14:36:44 +0700

    feat(composables): update: use-api for fetching data all types and type detail

---
+++ composables/api/useApiTypes.ts
@@ -0,0 +1,33 @@
+import type { ApiResponse } from "@/types/responses/ApiResponse"
+import type { Type } from "@/types/responses/Type"
+
+interface Types {
+  id: number
+  name: string
+  total_pokemon: number
+  single_pokemon: number
+}
+
+export function useApiTypes(){
+  const getTypes = async () => {
+    const { data } = await $fetch<ApiResponse<Types[]>>('/api/types', {
+      method: 'GET'
+    })
+
+    return { data }
+  }
+
+  const getType = async (name: string) => {
+    const { data } = await $fetch<ApiResponse<Type>>(`/api/types/${name}`, {
+      method: 'GET'
+    })
+
+    return data
+
+  }
+  
+  return {
+    getTypes,
+    getType
+  }
+}
