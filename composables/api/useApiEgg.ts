064c058f228a9978406f2aafccf29ca1f236eb32
Derida23 <arianderida@gmail.com>
2025-07-25 09:07:01 +0700

    feat(composables): add: use-egg to get egg list and egg detail

---
+++ composables/api/useApiEgg.ts
@@ -0,0 +1,30 @@
+import type { ApiResponse } from "@/types/responses/ApiResponse"
+import type { Eggs } from "~/types/responses/Eggs"
+
+interface EggsList {
+  id: number
+  name: string
+}
+
+export function useApiEgg(){
+  const getEggs = async () => {
+    const { data } = await $fetch<ApiResponse<EggsList[]>>('/api/eggs', {
+      method: 'GET'
+    })
+
+    return { data }
+  }
+
+  const getEgg = async (name: string) => {
+    const { data } = await $fetch<ApiResponse<Eggs>>(`/api/eggs/${name}`, {
+      method: 'GET'
+    })
+
+    return { data }
+  }
+
+  return {
+    getEggs,
+    getEgg
+  }
+}
