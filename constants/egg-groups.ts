6534f39098b60cffefdb6d5bdea7ac4c2cec04d6
Derida23 <arianderida@gmail.com>
2025-07-25 09:13:45 +0700

    feat(constant): add: egg-group primary data to load

---
+++ constants/egg-groups.ts
@@ -0,0 +1,62 @@
+export const eggs = [
+{
+"name": "monster",
+"id": 1
+},
+{
+"name": "water1",
+"id": 2
+},
+{
+"name": "bug",
+"id": 3
+},
+{
+"name": "flying",
+"id": 4
+},
+{
+"name": "ground",
+"id": 5
+},
+{
+"name": "fairy",
+"id": 6
+},
+{
+"name": "plant",
+"id": 7
+},
+{
+"name": "humanshape",
+"id": 8
+},
+{
+"name": "water3",
+"id": 9
+},
+{
+"name": "mineral",
+"id": 10
+},
+{
+"name": "indeterminate",
+"id": 11
+},
+{
+"name": "water2",
+"id": 12
+},
+{
+"name": "ditto",
+"id": 13
+},
+{
+"name": "dragon",
+"id": 14
+},
+{
+"name": "no-eggs",
+"id": 15
+}
+]
