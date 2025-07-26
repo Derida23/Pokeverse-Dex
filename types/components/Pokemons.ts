6a4017a22060b40c3d368a5ba06dbadb179dda9f
Derida23 <arianderida@gmail.com>
2025-07-26 11:14:37 +0700

    feat(types): add: types evolution and filter in evolotuin key

--- types/components/Pokemons.ts
+++ types/components/Pokemons.ts
@@ -1,3 +1,6 @@
 
 export type FilterKey = 'gen' | 'type' | 'q' | 'attr'
 export type FilterValue = string | number | null
+
+
+export type FilterEvo = 'gen' | 'type'
---
+++ types/responses/Evolution.ts
@@ -0,0 +1,28 @@
+export interface Evolution {
+  chain_id: number
+  evolves_to: EvolvesTo[]
+}
+
+export interface EvolvesTo {
+  detail: Detail
+  evolves_to: EvolvesTo2[]
+}
+
+export interface Detail {
+  item: string
+  level: string
+  name: string
+  type: number[]
+  id: number
+  generation: number
+}
+
+export interface EvolvesTo2 {
+  detail: Detail
+  evolves_to: EvolvesTo3[]
+}
+
+export interface EvolvesTo3 {
+  detail: Detail
+  evolves_to: []
+}
