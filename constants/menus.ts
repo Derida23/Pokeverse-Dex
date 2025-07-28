864229ef49fd87a3d5be1c00dff6044406e8852d
Derida23 <arianderida@gmail.com>
2025-07-28 13:28:31 +0700

    feat(constants): update: icon and similar in icones icon to have

--- constants/menus.ts
+++ constants/menus.ts
@@ -2,7 +2,7 @@ export const menus = [
   {
     title: 'Main Menu',
     menu: [
-      { name: 'Pokémons', icon: 'i-lucide-atom', slug: 'pokemons' },
+      { name: 'Pokémons', icon: 'i-ic-twotone-catching-pokemon', slug: 'pokemons' },
       { name: 'Compares', icon: 'i-lucide-git-compare-arrows', slug: 'compares' },
       { name: 'Statistics', icon: 'i-lucide-chart-pie', slug: 'statistics' },
       { name: 'Bookmarks', icon: 'i-lucide-bookmark-plus', slug: 'bookmarks' }
@@ -11,8 +11,8 @@ export const menus = [
   {
     title: 'Pokemon Data',
     menu: [
-      { name: 'Evolutions', icon: 'i-lucide-infinity', slug: 'evolutions' },
-      { name: 'Types', icon: 'i-lucide-toy-brick', slug: 'types' },
+      { name: 'Evolutions', icon: 'i-fluent-steps-16-filled', slug: 'evolutions' },
+      { name: 'Types', icon: 'i-material-symbols-dashboard-outline-rounded', slug: 'types' },
       { name: 'Egg Groups', icon: 'i-lucide-paw-print', slug: 'egg-groups' }
     ]
   },
@@ -20,7 +20,7 @@ export const menus = [
     title: 'Forms / Variations',
     menu: [
       { name: 'Gigantamax', icon: 'i-lucide-brain-cog', slug: 'pokemon-variations/gigantamax' },
-      { name: 'Mega Forms', icon: 'i-lucide-cannabis', slug: 'pokemon-variations/mega' },
+      { name: 'Mega Forms', icon: 'i-streamline-interface-weather-sun-photos-light-camera-mode-brightness-sun-photo-full', slug: 'pokemon-variations/mega' },
     ]
   }
 ];
