aa4519d920c4037ec0b57a9913e088b38387a1b7
Derida23 <arianderida@gmail.com>
2025-07-29 14:26:21 +0700

    feat(components): add: some component to reusable in pokemon detail pages

---
+++ components/Pokemon/Damage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  defineProps<{
+    damages: { name: string, id: string }[]
+    label: string
+    borderColor?: string
+    bgColor?: string
+  }>()
+</script>
+
+<template>
+  <section
+    v-if="damages.length > 0"
+    :class="[
+      'px-4 py-3 -ml-4 text-sm',
+      borderColor && `border-l-4 ${bgColor} ${borderColor}`
+    ]"
+  >
+    <p class="mb-2">{{ label }}</p>
+    <TypeBody :damages="damages" />
+  </section>
+</template>
---
+++ components/Pokemon/Dex.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+  data: {
+    type: Array as PropType<{title: string, description: string}[]>,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div v-for="item, id in props.data" :key="id">
+    <div class="flex mb-4 text-sm">
+      <h1 class="w-32 font-medium">{{ item.title }}:</h1>
+      <p>{{item.description }}</p>
+    </div>
+  </div>
+</template>
