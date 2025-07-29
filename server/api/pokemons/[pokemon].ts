94a09e46ae0de13bed20a704ddb26bf8bceccac7
Derida23 <arianderida@gmail.com>
2025-07-29 14:28:49 +0700

    feat(api): update: server api for endpoint pokemon detail

--- server/api/pokemons/[pokemon].ts
+++ server/api/pokemons/[pokemon].ts
@@ -1,79 +1,27 @@
 /* eslint-disable max-len */
 import { abilities } from "~/constants/abilities";
 import { chain_pokemons } from "~/constants/chain_pokemons";
-import { types } from "~/constants/types";
-import type { GenderRatio, PokemonId, Pokemons, Abilities } from "~/types/api/Pokemons";
-import type { PokemonSpecies, FlavorTextEntry } from "~/types/api/PokemonSpecies";
-import type { Type } from "~/types/responses/Types";
-import { calculateDefense, groupByEffectiveness } from "~/utils/damageTransform";
+import { pokemons } from "~/constants/pokemons";
+import type { PokemonId, Pokemons, Abilities } from "~/types/api/Pokemons";
+import type { PokemonSpecies } from "~/types/api/PokemonSpecies";
+import type { Pokemons as PokemonForm, Variants } from "~/types/responses/Pokemons";
+import { calculateGenderRatio, getType, getUniqueFlavorTexts, handleDefense } from "~/utils/pokemonApi";
 import { pokemonTransform } from "~/utils/pokemonTransform";
 import { getID } from "~/utils/textTransform";
 
-function calculateGenderRatio(genderRate: number): GenderRatio {
-  if (genderRate === -1) {
-    return {
-      male: 0,
-      female: 0,
-      isGenderless: true,
-    };
-  }
-
-  const female = (genderRate / 8) * 10;
-  const male = 10 - female;
-
-  return {
-    male: Math.floor(male),
-    female: Math.floor(female),
-    isGenderless: false,
-  };
-}
-
-function getUniqueFlavorTexts(entries: FlavorTextEntry[]) {
-  const seen = new Set();
-  const uniqueTexts = [];
-
-  for (const entry of entries) {
-    if (entry.language.name !== "en") continue;
-
-    const cleanedText = entry.flavor_text.replace(/\s+/g, " ").trim();
-
-    if (!seen.has(cleanedText)) {
-      seen.add(cleanedText);
-      uniqueTexts.push(cleanedText.toLowerCase());
-    }
-  }
-
-  return uniqueTexts.splice(0, 5);
-}
-
-const getType = (id: number) => {
-  return id ? types.find((item) => item.id === id)?.name : null
-}
-
-function handleDefense (main: Type, pair: Type) {
-  const defensed = calculateDefense(main.defense, pair.defense);
-  const grouped = groupByEffectiveness(defensed);
-  
-  const normalize = {
-    no_damage: grouped['0x'],
-    quarter_damage: grouped['0.25x'],
-    half_damage: grouped['0.5x'],
-    single_damage: grouped['1x'],
-    double_damage: grouped['2x'],
-    fourth_damage: grouped['4x'],
-  }
-  
-  return normalize
-}
-
-
 export default defineEventHandler(async (event) => {
   try {
+    const { form } = getQuery(event)
     const params = event.context.params?.pokemon;
     const pokemonId = pokemonTransform.find((item) => item.name === params) as PokemonId
-    
+    let pokemonFormId: PokemonId = pokemonId
+
+    if (form) {
+      pokemonFormId = pokemonTransform.find((item) => item.name === form) as PokemonId
+    } 
+
     const { apiUrl } = useRuntimeConfig()
-    const resPokemon = await $fetch<Pokemons>(`${apiUrl}/pokemon/${pokemonId.id}`, {
+    const resPokemon = await $fetch<Pokemons>(`${apiUrl}/pokemon/${pokemonFormId.id}`, {
       method: 'GET'
     })
 
@@ -83,7 +31,6 @@ export default defineEventHandler(async (event) => {
 
     const evolution= chain_pokemons.find((item) => item.chain_id === Number(getID(resSpecies.evolution_chain.url)))
     const text = getUniqueFlavorTexts(resSpecies.flavor_text_entries)
-    const specialAttack = resPokemon.stats.find((item) => item.stat.name === 'special-attack')
     const status = resPokemon.stats.map(item => { 
       return {
         base_stat: item.base_stat,
@@ -99,7 +46,7 @@ export default defineEventHandler(async (event) => {
       abilitiesPokemon.push({...abies, is_hidden: item.is_hidden})
     })
 
-    const [mainType, pairType] = pokemonId.type.map(getType)
+    const [mainType, pairType] = pokemonFormId.type.map(getType)
     const [main, pair] = await Promise.all([
       $fetch(`/api/types/${mainType}`),
       pairType ? $fetch(`/api/types/${pairType}`) : null
@@ -107,11 +54,27 @@ export default defineEventHandler(async (event) => {
 
     const defense = !pair ? main.data.defense : handleDefense(main.data, pair?.data ?? null)
 
+    const formPokemons = pokemons.find((item) => item.id === pokemonId.id) as PokemonForm
+    let forms = [] as Variants[]
+
+    if (formPokemons.form_variants) {
+        const dataForms = [{
+            form_id: formPokemons.id,
+            form_name: formPokemons.name,
+            japanese_name: formPokemons.japanese_name,
+            types: formPokemons.types[0]
+          },
+          ...formPokemons.form_variants
+        ]
+
+        forms = dataForms
+    }
+
     const pokemon = {
       detail: {
-        name: pokemonId.name,
-        id: pokemonId.id,
-        type: pokemonId.type,
+        name: pokemonFormId.name,
+        id: pokemonFormId.id,
+        type: pokemonFormId.type,
         height: resPokemon.height,
         weight: resPokemon.weight,
         gender: calculateGenderRatio(resSpecies.gender_rate),
@@ -120,11 +83,10 @@ export default defineEventHandler(async (event) => {
       evolution,
       text,
       dex: {
-        generation: pokemonId.generation,
-        habitat: resSpecies.habitat.name,
+        generation: pokemonFormId.generation,
+        habitat: resSpecies.habitat?.name ?? 'N/A',
         capture_rate: resSpecies.capture_rate,
         growth_rate: resSpecies.growth_rate.name,
-        ev_yield: specialAttack?.effort ?? 0,
         base_experience: resPokemon.base_experience,
         base_happiness: resSpecies.base_happiness
       },
@@ -135,9 +97,10 @@ export default defineEventHandler(async (event) => {
         egg_cycle: resSpecies.hatch_counter
       },
       type: {
-        id: pokemonId.type,
+        id: pokemonFormId.type,
         defense: defense
-      }
+      },
+      form: forms
     }
 
     return {
