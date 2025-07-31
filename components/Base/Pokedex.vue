4c5c5985503b9b877bcd89c587ed151895578621
Derida23 <arianderida@gmail.com>
2025-07-31 14:44:30 +0700

    feat(components): add: pokedex component to coverage all error

---
+++ components/Base/Pokedex.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable max-len -->
+<script setup lang="ts">
+defineProps<{
+  title: string,
+  description: string
+}>()
+</script>
+
+<!-- eslint-disable max-len -->
+<template>
+  <div class="w-full pt-16 flex-col flex-center">
+    <img src="@/assets/images/pokedex.png" alt="pokedex" class="w-56 z-10" >
+    <div class="bg-slate-100 border border-slate-400 rounded-lg -mt-20 w-96 min-h-56 p-3 pt-24 relative">
+      <p class="font-semibold mb-2">{{ title }}</p>
+      <p class="text-sm">{{ description }}</p>
+      <UIcon name="i-ic-twotone-catching-pokemon" class="text-xl absolute bottom-2 right-2 animate-bounce"/>
+    </div>
+  </div>
+</template>
