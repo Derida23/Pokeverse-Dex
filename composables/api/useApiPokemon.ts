78b1b6405c6c18abea0a490e7c54cc2a4b8e2603
Derida23 <arianderida@gmail.com>
2025-07-31 14:45:39 +0700

    feat(composables): add: get-compare api compares pages and integrated with server-api

--- composables/api/useApiPokemon.ts
+++ composables/api/useApiPokemon.ts
@@ -2,6 +2,7 @@ import type { FilterKey, FilterValue } from "@/types/components/Pokemons"
 import type { ApiResponse } from "~/types/api/ApiResponse"
 import type { Pokemons } from "@/types/responses/Pokemons"
 import type { PokemonDetail } from "~/types/api/PokemonDetail"
+import type { PokemonId } from "~/types/api/Pokemons"
 
 export function useApiPokemon(){
   const getPokemons = async (offset=0, limit=21, filter: Record<FilterKey, FilterValue>)
@@ -22,9 +23,17 @@ export function useApiPokemon(){
     )
   }
 
+  const getCompare = async (offset=0, limit=21, filter: Record<'q', string>)
+  : Promise<ApiResponse<PokemonId[]>> => {
+    return await $fetch(`/api/pokemon-variants?offset=${offset}&limit=${limit}`, 
+      { method: 'GET', query: filter }
+    )
+  }
+
   return {
     getAll,
     getPokemons,
-    getPokemon
+    getPokemon,
+    getCompare
   }
 }
