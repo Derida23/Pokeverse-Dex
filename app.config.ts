3fcc93b5125643b18c370c1482815d780ec89790
Derida23 <arianderida@gmail.com>
2025-08-20 17:15:08 +0700

    feat(config): update: nuxt ui divider object dark mode

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,10 @@ export default defineAppConfig({
   ui: {
     primary: 'sky',
     gray: 'zinc',
-  }
+    divider: {
+      border: {
+        base: 'flex border-gray-200 dark:border-gray-300',
+      }
+    }
+  },
 })
